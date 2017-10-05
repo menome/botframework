712ed67b29fb6685cbdef1a1b258594c6441fdd2
Konrad Aust <konrad.aust@gmail.com>
2017-10-04 23:56:03 -0600

    Added rabbit tests

--- test/rabbit.js
+++ test/rabbit.js
@@ -1,5 +1,39 @@
 var assert = require('chai').assert;
+var mock = require('mock-require');
+var simple = require('simple-mock');
+var amqp = require('amqplib');
+mock('amqplib', amqp); // Mock the neo4j driver.
+var rabbit = require('../src/rabbit')
+var logger = require('../src/logger');
+logger.logging = false;
 
-describe('Placeholder', function () {
-  it('Placeholder', function () {});
+describe('RabbitMQ', function () {
+  afterEach(function() {
+    simple.restore();
+  });
+
+  it('Connects to RabbitMQ', function (done) {
+    simple.mock(amqp, 'connect', () => {
+      return Promise.resolve({
+        on: () => {},
+        createChannel: () => {
+          return Promise.resolve({
+            assertExchange: () => {return Promise.resolve(true)}
+          })
+        }
+      })
+    })
+    
+    var rabbitClient = new rabbit({
+      url: 'url',
+      routingKey: 'key',
+      exchange: 'exchange',
+      exchangeType: 'exchangeType'
+    });
+    rabbitClient.connect();
+
+    assert.equal(amqp.connect.callCount,1)    
+    done();
+  });
+
 });
