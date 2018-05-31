8ac6e3d793388624443e3628a24c731bf50f4955
Konrad Aust <konrad.aust@gmail.com>
2018-05-31 11:38:28 -0600

    Less picky about swagger body inputs. Fixed date/time tests.

--- test/helpers.js
+++ test/helpers.js
@@ -17,36 +17,7 @@ describe('ParseProps', function () {
   });
 });
 
-describe('convertDate', function () {
-  it('Passes non-date parameters through.', function () {
-    var notDates = [
-      "asdf",
-      21.5,
-      "21.5",
-      "12345",
-      null,
-      true
-    ]
-    notDates.forEach((itm) => {
-      assert.equal(itm, helpers.convertDate(itm));
-    })
-  });
-
-  it('Converts string dates', function () {
-    var dates = [
-      {val: "01 Jan 1970 00:00:00 GMT", timestamp: 0 },
-      {val: "04 Dec 1995 00:12:00 GMT", timestamp: 818035920000 },
-      {val: "2015-03-25T12:00:00-06:30", timestamp: 1427308200000 },
-      {val: "10/15/2008 10:06:32 PM", timestamp: 1224129992000 },
-      {val: "2015-03-25 12:00:00.00", timestamp: 1427306400000 },
-      {val: "7/7/2011", timestamp: 1310018400000 },
-    ]
-
-    dates.forEach((itm) => {
-      assert.equal(helpers.convertDate(itm.val),itm.timestamp)
-    })
-  });
-
+describe('responsewrapper', function() {
   it('Can create a valid response message', function () {
     var message = helpers.responseWrapper({
       status: "success",
@@ -73,5 +44,36 @@ describe('convertDate', function () {
       otherKey: "Other key"
     }), Error)
   });
+})
+
+describe('convertDate', function () {
+  it('Passes non-date parameters through.', function () {
+    var notDates = [
+      "asdf",
+      21.5,
+      "21.5",
+      null,
+      true
+    ]
+    notDates.forEach((itm) => {
+      assert.equal(itm, helpers.convertDate(itm));
+    })
+  });
+
+  it('Converts string dates', function () {
+    var dates = [
+      {val: "01 Jan 1970 00:00:00 GMT", timestamp: "1970-01-01T00:00:00.000Z" },
+      {val: "04 Dec 1995 00:12:00 GMT", timestamp: "1995-12-04T00:12:00.000Z" },
+      {val: "2015-03-25T12:00:00-06:30", timestamp: "2015-03-25T18:30:00.000Z" },
+      {val: "10/15/2008 10:06:32 PM", timestamp: "2008-10-16T04:06:32.000Z" },
+      {val: "2015-03-25 12:00:00.00", timestamp: "2015-03-25T18:00:00.000Z" },
+      {val: "7/7/2011", timestamp: "2011-07-07T06:00:00.000Z" },
+    ]
+
+    dates.forEach((itm) => {
+      assert.equal(helpers.convertDate(itm.val),itm.timestamp)
+    })
+  });
+
 });
 
