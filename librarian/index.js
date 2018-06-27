9c2f81cf0465376324429dfbaf4ee102c2b8f94e
Konrad Aust <konrad.aust@gmail.com>
2018-06-27 12:56:08 -0600

    Added stat method for librarian

--- librarian/index.js
+++ librarian/index.js
@@ -4,9 +4,33 @@
 
 const fs = require('fs');
 const request = require('request');
+const rp = require('request-promise');
 const URL = require('url').URL;
 
 module.exports = function(config) {
+  // Calls HEAD to see if the file exists.
+  this.stat = (key, path) => {
+    var url =  new URL('/file', config.host);
+    
+    url.searchParams.set("library",key);
+    url.searchParams.set("path",path);
+
+    var opts = {
+      method: "HEAD",
+      url: url.toString(),
+      qs: {
+        library: key,
+        path: path
+      },
+      auth: {
+        user: config.username,
+        pass: config.password
+      }
+    }
+
+    return rp(opts);
+  }
+
   this.download = (key,path,filePath) => {
     var url =  new URL('/file', config.host);
     
