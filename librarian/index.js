ebae299cdc600b8d625f256e8dc1d7c91d0ba133
Konrad Aust <konrad.aust@gmail.com>
2019-05-24 15:37:58 -0600

    Fixed event name

--- librarian/index.js
+++ librarian/index.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
         .on('error', (err) => {
           return reject(err);
         })
-        .on('end', () => {
+        .on('finish', () => {
           return resolve(filePath);
         })
     });
